3e65dedb698a6c2f777acd6613043a8d8066d5c5
Maria <masya757575@yandex.ru>
2026-06-06 17:24:19 +0300

    update HW8 with fix logic in User and homeWorkReducer and style

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+        const sortedUpState = [...state].sort((a, b) =>
+          a.name.localeCompare(b.name),
+        );
+        return sortedUpState;
+      } else if (action.payload === "down") {
+        const sortedDownState = [...state].sort((a, b) =>
+          b.name.localeCompare(a.name),
+        );
+        return sortedDownState;
+      }
+      return state;
     }
-}
+    case "check": {
+      return [...state].filter((el) => el.age >= action.payload);
+      //return state // need to fix
+    }
+    default:
+      return state;
+  }
+};
